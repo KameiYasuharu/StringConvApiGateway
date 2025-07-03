05abec37d3fe61d2b834fc3ebea44de5031358b1
KameiYasuharu <kameiyasuharu@gmail.com>
2025-07-03 14:50:19 +0800

    Update application_start.sh
    Update application_start.sh

--- application_start.sh
+++ application_start.sh
@@ -3,5 +3,5 @@
 echo `/bin/date "+%Y-%m-%d %H:%M:%S"` "INFO application_start.sh 実行"
 
 echo `/bin/date "+%Y-%m-%d %H:%M:%S"` "INFO Spring Boot 起動"
-java -jar /usr/local/tomcat/webapps/*.war -XX:MaxRAMPercentage=80.0
+java -jar /usr/webapps/*.war -XX:MaxRAMPercentage=80.0
 
